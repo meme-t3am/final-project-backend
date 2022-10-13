28135a35bccb4ca9b2082640c7f754a9974a9972
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-10-13 10:26:58 -0700

    set up template

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,5 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS todos;
-
-DROP TABLE IF EXISTS tasks;
-
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -12,12 +8,4 @@ CREATE TABLE users (
   password_hash VARCHAR NOT NULL,
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL
-);
-
-CREATE TABLE todos (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  user_id BIGINT,
-  completed BOOLEAN NOT NULL DEFAULT(FALSE),
-  description VARCHAR,
-  FOREIGN KEY (user_id) REFERENCES users(id)
 );
