21202814ba177afade0ac7e80dbb46342f135559
Niki Hite <nikihite@comcast.net>
2022-10-17 09:41:49 -0700

    added mungeData function

--- APIServices.js
+++ APIServices.js
@@ -1,5 +1,40 @@
 const fetch = require('cross-fetch');
 
+const data = {
+  'result': {
+    'tags': [
+      {
+        'confidence': 98.4662017822266,
+        'tag': {
+          'en': 'book jacket'
+        }
+      },
+      {
+        'confidence': 77.58984375,
+        'tag': {
+          'en': 'jacket'
+        }
+      },
+      {
+        'confidence': 58.2133979797363,
+        'tag': {
+          'en': 'wrapping'
+        }
+      },
+      {
+        'confidence': 38.989315032959,
+        'tag': {
+          'en': 'covering'
+        }
+      }
+      
+    ]
+  },
+  'status': {
+    'text': '',
+    'type': 'success'
+  }
+};
 
 async function imaggaAPI(imgURL) {
 
@@ -17,7 +52,17 @@ async function imaggaAPI(imgURL) {
 }
 
 
-
+function mungeData(data) {
+  const result = data.result.tags.map((datum) => {
+    const newObj = {
+      confidence: datum.confidence,
+      tag: datum.tag.en
+    };
+    return newObj;
+  });
+  return result;
+}
+mungeData(data);
 
 async function addMemes(memeUrlArray) {
   const promises = memeUrlArray.map((url) => imaggaAPI(url));
